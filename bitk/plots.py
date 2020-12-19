48b2910836818141942f8aac4b48284839b551ab
Logan <yuelong.chen.btr@gmail.com>
2020-12-19 15:39:46 +0800

    [M]: scatter edited

--- bitk/plots.py
+++ bitk/plots.py
@@ -24,12 +24,15 @@ def compare_plots(df,column1,column2,name1,name2,value_name,output,hue):
     '''
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(18, 9), tight_layout=True)
     jp = sns.scatterplot(x=column1, y=column2, data=df,ax=ax1,hue=hue)
+    lim=max(df[column1].max(),df[column2].max())
     rm_na_df = df[[column1,column2]].dropna()
     cor,p = stats.pearsonr(rm_na_df[column1],rm_na_df[column2])
-    jp.annotate(text='COR=%.2f,p=%.2f' % (cor,p) ,xy=(0,max(df[column2])))
+    jp.annotate(text='COR=%.2f,p=%.2f' % (cor,p) ,xy=(0,lim),xycoords='data')
     jp.set_xlabel(name1)
     jp.set_ylabel(name2)
     jp.set_title(f'Scatter plots of {value_name} in {name1} and {name2}')
+    jp.set_xlim(0,lim+1)
+    jp.set_ylim(0,lim+1)
     tmp=df.rename(columns={column1:name1,column2:name2}).melt(id_vars=None,
                                                               value_vars=[name1,name2],
                                                               var_name='class',
